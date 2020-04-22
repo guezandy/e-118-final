f174831fead6c43c4d80ef34cfb5dac7ab0b119e
Andrew Rodriguez <andrewr@twitter.com>
2020-04-22 13:37:55 -0700

    Final  UI updates and getting deploying to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,21 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic = 'plug vendor bracket treat month trophy opinion adjust fatigue apple rigid hat';
+
 module.exports = {
-  // Uncommenting the defaults below 
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
-  //
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545, // Ganache UI
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/0ebda51d1e5344349355d3dbd9a5c468')
+      },
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  }
 };
